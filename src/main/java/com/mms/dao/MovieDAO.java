ea7ac52193e1aa674276138bb45426fd89ed0154
Purushothaman <91775559+Purushothaman1962@users.noreply.github.com>
2024-01-19 16:25:10 +0530

    Updated MovieDAO.java

--- src/main/java/com/mms/dao/MovieDAO.java
+++ src/main/java/com/mms/dao/MovieDAO.java
@@ -1,8 +1,5 @@
 package com.mms.dao;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.mms.bean.Movie;
